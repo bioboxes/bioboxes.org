9c31c3ef32635186b67f9b0fffc5c61ec55eaf12
Michael Barton <mail@michaelbarton.me.uk>
2015-03-30 18:00:37 -0700

    Build list of guides from data file
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- extensions/page.rb
+++ extensions/page.rb
@@ -8,9 +8,12 @@ class PageTitle < Middleman::Extension
   def manipulate_resource_list(resources)
     resources.each do |resource|
       if is_index? resource
-        resource.raw_data['title'] = "Bioboxes"
+        resource.raw_data['page_title'] = "Bioboxes"
       elsif is_markdown? resource
-        resource.raw_data['title'] = "Bioboxes — " + fetch_title(resource.source_file)
+        resource.raw_data['title'] = fetch_title(resource.source_file)
+        resource.raw_data['page_title'] = "Bioboxes — " + resource.raw_data['title']
+
+        resource.raw_data['summary'] = fetch_first_paragraph(resource.source_file)
       end
     end
   end
@@ -19,6 +22,10 @@ class PageTitle < Middleman::Extension
     line = File.readlines(path).detect{|i| i =~ /^# /}
     raise ArgumentError, "No title set for #{path}" if line.nil?
     line.gsub("# ", "").titleize
+  end
+
+  def fetch_first_paragraph(path)
+    File.read(path).split("\n\n", 2)[1]
   end
 
   def is_index?(resource)
