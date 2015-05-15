de5ca9f6ab43948c6f8fe1e1283147460c679055
PlsDontPanic <pbelmann@cebitec.uni-bielefeld.de>
2015-05-15 12:38:08 +0200

    partial code view added

--- helpers/code_helpers.rb
+++ helpers/code_helpers.rb
@@ -3,11 +3,13 @@ require "open-uri"
 module CodeHelpers
 
   GITHUB = 'https://raw.githubusercontent.com/'
+  MAX_FILE_LENGTH = 100000
 
-  def github_code(repo, file, language)
+  def github_code(repo, file, language, fileStart = 0, fileEnd = MAX_FILE_LENGTH)
     url  = File.join(GITHUB, repo, "master", file)
-    code = URI.parse(url).read
-    ["~~~ #{language}", code, "~~~"].join("\n")
+    codeLines = URI.parse(url).read.lines.to_a
+    codeBlock = fileStart < codeLines.length ? codeLines[fileStart .. fileEnd].join('') : ""
+    ["~~~ #{language}", codeBlock, "~~~"].join("\n")
   end
 
 end
