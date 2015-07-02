60d997931805ae613c21bead3ee400f89eb8fb31
Michael Barton <mail@michaelbarton.me.uk>
2015-07-02 14:17:46 +0100

    Add missing semicolon in js
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- source/javascripts/_bioboxes.js
+++ source/javascripts/_bioboxes.js
@@ -1,6 +1,6 @@
 hljs.initHighlightingOnLoad();
 
-var metrics_url = "http://metrics.bioboxes.org/v1/containers.json"
+var metrics_url = "http://metrics.bioboxes.org/v1/containers.json";
 
 $.getJSON(metrics_url, function(json) {
   var downloads = _.last(json['total']['value']);
